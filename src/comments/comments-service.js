7e6265af70b3db50b68d6203c63d1e49eb8b1a96
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-27 17:55:21 -0600

    Add sterilizedComment to comments-service

--- src/comments/comments-service.js
+++ src/comments/comments-service.js
@@ -1,3 +1,5 @@
+const xss = require("xss");
+
 const CommentsService = {
   getById(knex, id) {
     return (
@@ -17,6 +19,17 @@ const CommentsService = {
       .then(([comment]) => comment)
       .then(comment => CommentsService.getById(db, comment.id));
   }
+};
+
+sterilizedComment = comment => {
+  const { user } = comment;
+  return {
+    id: comment.id,
+    content: xss(comment.content),
+    site_id: comment.site_id,
+    user_ref: user.user_name,
+    date_posted: new Date(comment.date_posted)
+  };
 };
 
 module.exports = CommentsService;
