64952a546e4dc01660992395418dd1c50cbac8dc
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-27 14:54:38 -0600

    Add test DB to config.js

--- src/config.js
+++ src/config.js
@@ -2,5 +2,8 @@ module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
   DB_URL:
-    process.env.DB_URL || "postgresql://dunder_mifflin@localhost/trash_tracker"
+    process.env.DB_URL || "postgresql://dunder_mifflin@localhost/trash_tracker",
+  TEST_DB_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://dunder_mifflin@localhost/trash_tracker-test"
 };
