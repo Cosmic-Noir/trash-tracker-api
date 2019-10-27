2f385f7788dd6a4ccc204bf344709f6d2c610e0b
Kristen Doucette <cosmicnoir90@gmail.com>
2019-10-27 15:08:47 -0600

    Create and update comments-service.js

---
+++ src/comments/comments-service.js
@@ -0,0 +1,20 @@
+const CommentsService = {
+  getById(knex, id) {
+    return (
+      knex
+        .from("tt_comments")
+        .select("*")
+        .where("userRef", id)
+        //Unclear if needed below:
+        .first()
+    );
+  },
+  insertComment(knex, newComment) {
+    return knex
+      .insert(newComment)
+      .into("tt_comments")
+      .returning("*")
+      .then(([comment]) => comment)
+      .then(comment => CommentsService.getById(db, comment.id));
+  }
+};
