e9548d6814a58bb257f2a8e98c84012132d5fdc1
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-02 14:14:14 -0600

    Remove extra deleteSite

--- src/sites/sites-service.js
+++ src/sites/sites-service.js
@@ -30,11 +30,6 @@ const SitesService = {
       .where("id", id)
       .first();
   },
-  deleteSite(knex, id) {
-    return knex("tt_sites")
-      .where({ id })
-      .delete();
-  },
   getCommentsForSite(knex, site_id) {
     return knex
       .from("tt_comments")
