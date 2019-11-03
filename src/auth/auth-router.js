626a256bae272a0bd3592c302ba43da3b639a25d
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-03 11:55:00 -0700

    Create jwt response for valid credentials on log in

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -7,6 +7,7 @@ authRouter.post("/", jsonParser, (req, res, next) => {
   const { username, pass } = req.body;
   const loginUser = { username, pass };
 
+  // Check missing username or pass in req
   for (const [key, value] of Object.entries(loginUser)) {
     if (value == null) {
       return res.status(400).json({
@@ -14,12 +15,14 @@ authRouter.post("/", jsonParser, (req, res, next) => {
       });
     }
   }
+  // Check user exists
   AuthService.getUser(req.app.get("db"), loginUser.username)
     .then(dbUser => {
       if (!dbUser)
         return res.status(400).json({
           error: "Incorrect username or password"
         });
+      // Check password matches
       return AuthService.comparePasswords(loginUser.pass, dbUser.pass).then(
         compareMatch => {
           if (!compareMatch) {
@@ -27,7 +30,12 @@ authRouter.post("/", jsonParser, (req, res, next) => {
               error: "Incorrect username or password"
             });
           }
-          res.send("ok");
+          // Send jwt if all passes
+          const sub = dbUsert.username;
+          const payload = { user_id: dbUser.id };
+          res.send({
+            authToken: AuthService.createJwt(sub, payload)
+          });
         }
       );
     })
