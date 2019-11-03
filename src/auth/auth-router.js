a38f01c728cbc328b64beacf917f7b9eccc87a11
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-03 11:45:21 -0700

    Add check for username and pass in req body

--- src/auth/auth-router.js
+++ src/auth/auth-router.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const jsonParser = express.json();
 
 const authRouter = express.Router();
 
-authRouter.post("/", (req, res, next) => {
+authRouter.post("/", jsonParser, (req, res, next) => {
+  const { username, pass } = req.body;
+  const loginUser = { username, pass };
+
+  for (const [key, value] of Object.entries(loginUser)) {
+    if (value == null) {
+      return res.status(400).json({
+        error: `Missing '${key}' in request body`
+      });
+    }
+  }
+
   res.send("ok");
 });
 
