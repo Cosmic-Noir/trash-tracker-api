6ce152d2d8c2304f286db385d9fc9a8ce66976a4
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-05 19:38:35 -0700

    Add deleteComment to comment-service

--- src/comments/comments-service.js
+++ src/comments/comments-service.js
@@ -15,6 +15,11 @@ const CommentsService = {
       .then(([comment]) => comment)
       .then(comment => CommentsService.getById(knex, comment.id));
   },
+  deleteComment(knex, id) {
+    return knex("tt_comments")
+      .where({ id })
+      .delete();
+  },
   sterilizedComment(comment) {
     return {
       id: comment.id,
