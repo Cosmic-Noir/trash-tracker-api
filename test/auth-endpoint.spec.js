c9a5c0e4df8c114a236b21e9c00a3fff6d53afcb
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-15 10:20:31 -0700

    Add fixtures to auth-endpoints.spec.js

--- test/auth-endpoint.spec.js
+++ test/auth-endpoint.spec.js
@@ -1,5 +1,12 @@
 const knex = require("knex");
 const app = require("../src/app");
+const jwt = require("jsonwebtoken");
+const { makeUsersArray, makeAuthHeader } = require("./users.fixtures");
+const {
+  makeTrashSitesArray,
+  makeCleanSitesArray,
+  makeMaliciousSite
+} = require("./sites.fixtures");
 
 describe("Auth Endpoints", function() {
   let db;
