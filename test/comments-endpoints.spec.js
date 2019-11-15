c1f8bf0bb7adbc327fc63295414d9c780edd2d54
Kristen Doucette <cosmicnoir90@gmail.com>
2019-11-15 08:41:15 -0700

    Import required test data and set up connection to test db in comments-endpoints.spec.js

--- test/comments-endpoints.spec.js
+++ test/comments-endpoints.spec.js
@@ -1,5 +1,36 @@
 const { expect } = require("chai");
 const knex = require("knex");
 const app = require("../src/app");
+const {
+  makeMaliciousComment,
+  makeCommentsArray
+} = require("./comments.fixtures");
+const { makeUsersArray, makeAuthHeader } = require("./users.fixtures");
+const {
+  makeTrashSitesArray,
+  makeCleanSitesArray,
+  makeMaliciousSite
+} = require("./sites.fixtures");
+
+let db;
+
+// Create connection to test DB:
+
+before("Make knex instance", () => {
+  db = knex({
+    client: "pg",
+    connection: process.env.TEST_DB_URL
+  });
+  app.set("db", db);
+});
+
+// Disconnect and clear the table for testing
+after("Disconnect from db", () => db.destroy());
+before("Clear the table", () =>
+  db.raw("TRUNCATE tt_comments, tt_sites, tt_users RESTART IDENTITY CASCADE")
+);
+afterEach("Cleanup", () =>
+  db.raw("TRUNCATE tt_comments, tt_sites, tt_users RESTART IDENTITY CASCADE")
+);
 
 describe("Comments Endpoints", function() {});
